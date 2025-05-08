b79422dbeb0c0eae3a8e5d4750913103d5569d60
bmunguia <bmunguia@stanford.edu>
2025-05-08 11:28:21 -0700

    add sample metric to test

--- tests/read_write_mesh.py
+++ tests/read_write_mesh.py
@@ -12,13 +12,25 @@ meshpath_out = "output/quad.meshb"
 pymeshb.write_mesh(meshpath_out, coords, elements,
                    solution=solution)
 
+# Create a sample solution dict
+num_point = coords.shape[0]
+num_dim = coords.shape[1]
+solution = {
+    "Temperature": coords[:, 0] + 10,
+    "Velocity": np.zeros((num_point, num_dim)),
+    "Metric": np.zeros((num_point, (num_dim * (num_dim + 1)) // 2))
+}
+solution["Velocity"][:, 1] = coords[:, 1] * 2
+
+# Make sample metric diagonal
+# We store the lower triangle of the tensor
+solution["Metric"][:, 0] = 1e1
+solution["Metric"][:, 2] = 1e2
+solution["Metric"][:, 5] = 1e3
+
 # Write the mesh with solution to a new file
 meshpath_out = "output/quad_with_sol.meshb"
 solpath_out = "output/quad_with_sol.solb"
-solution = {
-    "Temperature": coords[:, 0] + 10,
-    "Velocity": np.zeros((coords.shape[0], coords.shape[1])),
-}
-solution["Velocity"][:, 1] = coords[:, 1] * 2
+
 pymeshb.write_mesh(meshpath_out, coords, elements,
                    solpath=solpath_out, solution=solution)
